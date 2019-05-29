53479a0dac696a4b94a7b4056ee24b3843544ed5
AmineOthman <amineothmene@gmail.com>
2019-05-29 22:28:06 +0100

    add javascropt

---
+++ Newcreation.js
@@ -0,0 +1,11 @@
+
+function categ(){
+    let categorie=document.getElementById('categorie');
+    let pneu=document.getElementById('pneu-cat');
+    
+
+
+    if (categorie.value == "Pneu"){
+        pneu.style.display= 'flex'
+   }
+}
